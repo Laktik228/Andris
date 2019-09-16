b9f846d2077ce846a55cc07bb3fd58ddfa382310
Laktik228 <andrew.wushu@gmail.com>
2019-09-16 12:49:17 +0100

    Setting up the scene, random spawning of tetris items on the spawnpoints, background music set up

---
+++ Assets/Background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public SpriteRenderer BackgroundSprite;
+    void Awake()
+    {
+        BackgroundSprite = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(BackgroundSprite);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SpawningTetrisItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningTetrisItem : MonoBehaviour
+{
+    public GameObject[] tetrisItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int IndexOfItem = Random.Range(0, tetrisItem.Length);
+        tetrisItem[IndexOfItem].transform.GetChild(0).position = transform.position;
+        Instantiate(tetrisItem[IndexOfItem]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
